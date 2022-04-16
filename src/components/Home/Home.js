42aad18fada5be975e72cfa994a18bf0c1206a04
Rasel-ahemd <nhd305812@gmail.com>
2022-04-16 20:50:40 +0500

    home page done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import H from "./Home.module.css";
+import DoctorImg from "../../img/man-with-psychologist-in-the-office-62XB78R1.jpg";
+const Home = () => {
+  return (
+    <div className={H.container}>
+      <div className={H.homeText}>
+        <h1>
+          <span>D.Alia</span>Private mental health consultation from a
+          professional
+        </h1>
+        <p>
+          For over 15 years, Talkingminds have been a respected provider of
+          psychology and clinical psychology, counselling, psychotherapeutic,
+          training and life coaching services in Sydney. Operating in the inner
+          west/CBD fringe suburb of Ultimo, Sydney and in a secondary locations
+          in Camden (south west Sydney/Macarthur region) and Stanmore our
+          practice has provided easy access to high quality, affordable services
+        </p>
+        <button>Learn more..</button>
+      </div>
+      <div className={H.homeImg}>
+        <img src={DoctorImg} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
