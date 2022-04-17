59e69ec73620be863bed5565e69c40be0f3feb29
Rasel-ahemd <nhd305812@gmail.com>
2022-04-17 05:20:50 +0500

    firebase deploy

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import About from "./components/About/About";
 import ResetPass from "./components/ResetPass";
+import Notfound from "./components/Notfound/Notfound";
 
 function App() {
   return (
@@ -36,6 +37,7 @@ function App() {
           <Route path="/about" element={<About></About>}></Route>
           <Route path="/blog" element={<Blog></Blog>}></Route>
           <Route path="/reset" element={<ResetPass></ResetPass>}></Route>
+          <Route path="*" element={<Notfound></Notfound>}></Route>
         </Routes>
 
         <Footer></Footer>
---
+++ src/components/Notfound/Notfound.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Notfound = () => {
+  return (
+    <div
+      style={{
+        padding: "50px 10px",
+        width: "300px",
+        margin: "auto",
+        textAlign: "center",
+      }}
+    >
+      <h1>Page not founded</h1>
+      <h1>404</h1>
+    </div>
+  );
+};
+
+export default Notfound;
