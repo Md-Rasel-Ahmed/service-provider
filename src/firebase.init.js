86ff62b653e0b34945fdda0a4461b4c329f673a6
Rasel-ahemd <nhd305812@gmail.com>
2022-04-18 12:42:28 +0500

    env file create

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDp-A111dfk1U10YDhErvmHHyOvJTZlFFs",
-  authDomain: "talking-mind-firebase.firebaseapp.com",
-  projectId: "talking-mind-firebase",
-  storageBucket: "talking-mind-firebase.appspot.com",
-  messagingSenderId: "51375075927",
-  appId: "1:51375075927:web:86e829320859644cb52912",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
